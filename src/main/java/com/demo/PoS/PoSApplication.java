1e4de94ec66207c6e2342fe9e3c0e72fb6e89d1e
Aleksandra Kondratjeva <artnaxel@outlook.com>
2023-12-07 21:33:17 +0200

    project init

---
+++ src/main/java/com/demo/PoS/PoSApplication.java
@@ -0,0 +1,13 @@
+package com.demo.PoS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoSApplication.class, args);
+	}
+
+}
