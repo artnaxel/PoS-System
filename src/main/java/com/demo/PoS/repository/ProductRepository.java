e6ba2a80f40e9b66144aa82149710f3840a0fd49
Aleksandra Kondratjeva <artnaxel@outlook.com>
2024-01-09 22:42:39 +0200

    adjust logic of delete discount

--- src/main/java/com/demo/PoS/repository/ProductRepository.java
+++ src/main/java/com/demo/PoS/repository/ProductRepository.java
@@ -9,4 +9,6 @@ import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     List<Product> findByLoyaltyProgram(LoyaltyProgram loyaltyProgram);
+
+    List<Product> findByDiscount_Id(UUID discountId);
 }
